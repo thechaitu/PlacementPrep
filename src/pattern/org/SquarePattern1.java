d623e225dbe6b327b58bd39ea3ec7cb07b335ee3
thechaitu <chaitranatekar79@gmail.com>
2025-02-07 21:23:45 +0530

    first pattern

---
+++ src/pattern/org/SquarePattern1.java
@@ -0,0 +1,16 @@
+package pattern.org;
+
+public class SquarePattern1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1;i<=5;i++) {
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
